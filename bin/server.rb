52f1acd9a614d2adae372ca33c7c6b2c40666867
aroswell <aroswell@gmail.com>
2016-01-20 22:30:36 -0500

    Commented out 'rerun' require command.

--- bin/server.rb
+++ bin/server.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 require "thin"
 require 'tilt/erb'
-require 'rerun'
+# require 'rerun'
 
 require File.expand_path("../../config/initializers/setup_env", __FILE__)
 require File.expand_path("../../config/routes", __FILE__)
